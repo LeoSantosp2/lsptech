36377f8289458390b36779d7434a28804822ee7c
Leonardo Santos <leogilberto2010@hotmail.com>
2023-02-21 19:19:09 -0300

    Pequenas correções

--- front-end/src/store/modules/auth/sagas.js
+++ front-end/src/store/modules/auth/sagas.js
@@ -1,18 +1,27 @@
 import { call, put, all, takeLatest } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
+import { get } from 'lodash';
 
 import * as actions from './actions';
 import * as types from '../types';
 
 import axios from '../../../services/axios';
 
-function* loginRequest({ payload }) {
+export function* loginRequest({ payload }) {
     try {
         const response = yield call(axios.post, '/token', payload);
 
         yield put(actions.loginSuccess({ ...response.data }));
 
         toast.success('Login Efetuado.');
+
+        /*
+        axios.interceptors.request.use((config) => {
+            const token = response.data.token;
+            config.headers.Authorization = `Bearer ${token}`;
+            return config;
+        });
+        */
 
         axios.defaults.headers.Authorization = `Bearer ${response.data.token}`;
     } catch (e) {
@@ -24,4 +33,18 @@ function* loginRequest({ payload }) {
     }
 }
 
-export default all([takeLatest(types.LOGIN_REQUEST, loginRequest)]);
+export function persistRehydrate({ payload }) {
+    const token = get(payload, 'auth.token', '');
+
+    if (!token) return;
+
+    axios.interceptors.request.use((config) => {
+        config.headers.Authorization = `Bearer ${token}`;
+        return config;
+    });
+}
+
+export default all([
+    takeLatest(types.LOGIN_REQUEST, loginRequest),
+    takeLatest(types.PERSIST_REHYDRATE, persistRehydrate),
+]);
